ed7c26784990164687bbbf53a6120ef27c8dedb4
Piyush Suthar <32237558+PiyushSuthar@users.noreply.github.com>
2021-06-25 21:40:33 +0530

    :tada: Project initialised

---
+++ vite.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite'
+import prefresh from '@prefresh/vite'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  esbuild: {
+    jsxFactory: 'h',
+    jsxFragment: "Fragment",
+    jsxInject: 'import {h, Fragment } from "preact"'
+  },
+  plugins: [prefresh()],
+})
