91b77335a7189703d25f2796ccfb9398f39d0f42
Piyush Suthar <32237558+PiyushSuthar@users.noreply.github.com>
2021-06-25 21:43:16 +0530

    :art: added: base files

---
+++ src/Views/Desktop/index.tsx
@@ -0,0 +1,24 @@
+import styles from "./Desktop.module.css";
+import default_dark_bg from "../../assets/wallpapers/default_dark.jpg";
+import default_light_bg from "../../assets/wallpapers/default_light.jpg";
+import { TaskBar } from "../../Components/Taskbar/";
+import { ContextMenu } from "../../Components/ContextMenu";
+import { useRef } from "preact/hooks";
+
+interface Props {}
+
+export const Desktop = (props: Props) => {
+  const ContainerRef = useRef<HTMLDivElement>();
+  return (
+    <div
+      class={styles.container}
+      ref={ContainerRef}
+      style={{
+        "--bg-image": `url(${default_light_bg})`,
+      }}
+    >
+      <ContextMenu containerRef={ContainerRef} />
+      <TaskBar />
+    </div>
+  );
+};
---
+++ src/main.tsx
@@ -0,0 +1,5 @@
+import "./index.css";
+import { Desktop } from "./Views/Desktop";
+import { render } from "preact";
+
+render(<Desktop />, document.getElementById("root")!);
