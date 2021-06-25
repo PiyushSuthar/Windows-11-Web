91af2eb05e100c87d0a803c74df6795a30bd89f3
Piyush Suthar <32237558+PiyushSuthar@users.noreply.github.com>
2021-06-25 22:07:18 +0530

    :adhesive_bandage: fixed: asset handling issue

--- src/Views/Desktop/index.tsx
+++ src/Views/Desktop/index.tsx
@@ -1,6 +1,6 @@
 import styles from "./Desktop.module.css";
-import default_dark_bg from "../../assets/wallpapers/default_dark.jpg";
-import default_light_bg from "../../assets/wallpapers/default_light.jpg";
+// import default_dark_bg from "../../assets/wallpapers/default_dark.jpg";
+// import default_light_bg from "../../assets/wallpapers/default_light.jpg";
 import { TaskBar } from "../../Components/Taskbar/";
 import { ContextMenu } from "../../Components/ContextMenu";
 import { useRef } from "preact/hooks";
@@ -13,9 +13,9 @@ export const Desktop = (props: Props) => {
     <div
       class={styles.container}
       ref={ContainerRef}
-      style={{
-        "--bg-image": `url(${default_light_bg})`,
-      }}
+      // style={{
+      //   "--bg-image": `url(${default_light_bg})`,
+      // }}
     >
       <ContextMenu containerRef={ContainerRef} />
       <TaskBar />
