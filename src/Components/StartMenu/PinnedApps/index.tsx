0498d58b4528cdf9cbd346dad9cc398b37877021
Piyush Suthar <32237558+PiyushSuthar@users.noreply.github.com>
2021-07-07 22:58:01 +0530

    :alembic: added: Pinned apps in start menu

--- src/Components/StartMenu/PinnedApps/index.tsx
+++ src/Components/StartMenu/PinnedApps/index.tsx
@@ -1,3 +1,7 @@
+import {
+  PinnedApp,
+  PinnedApps as PinnedAppsArray,
+} from "../../../Configs/startMenu.config";
 import styles from "./PinnedApps.module.css";
 
 export const ChevronIcon = ({ size = "24" }) => (
@@ -16,22 +20,51 @@ export const ChevronIcon = ({ size = "24" }) => (
     <polyline points="9 18 15 12 9 6"></polyline>
   </svg>
 );
-export const PinnedApps = () => {
-  const Button = ({ children }: { children: string }) => (
-    <button class={styles.button}>
-      {children} <ChevronIcon size="14" />
-    </button>
-  );
 
+const App = ({ name, icon, action, isLink, url }: PinnedApp) => {
+  const RequiresWrapper = () => (
+    <div onClick={action && action} class={styles.app}>
+      <img src={icon} alt={name} />
+      <p>{name}</p>
+    </div>
+  );
+  return isLink ? (
+    <a class={styles.link} href={url} target="_blank" rel="noopener noreferrer">
+      <RequiresWrapper />
+    </a>
+  ) : (
+    <RequiresWrapper />
+  );
+};
+
+export const Button = ({ children }: { children: string }) => (
+  <button class={styles.button}>
+    {children} <ChevronIcon size="14" />
+  </button>
+);
+
+interface TitleProps {
+  title: string;
+  button: string;
+}
+export const TitleArea = ({ title, button }: TitleProps) => (
+  <div class={styles.title_area}>
+    <div className="title">
+      <p>{title}</p>
+    </div>
+    <div className="title_button">
+      <Button>{button}</Button>
+    </div>
+  </div>
+);
+export const PinnedApps = () => {
   return (
     <div class={styles.container}>
-      <div class={styles.title_area}>
-        <div className="title">
-          <p>Pinned</p>
-        </div>
-        <div className="title_button">
-          <Button>All Apps</Button>
-        </div>
+      <TitleArea title="Pinned" button="All Apps" />
+      <div class={styles.apps}>
+        {PinnedAppsArray.map((pinnedapp, index) => (
+          <App {...pinnedapp} key={pinnedapp.name} />
+        ))}
       </div>
     </div>
   );
