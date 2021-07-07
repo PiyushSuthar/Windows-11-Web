06959d097f1e200248651c9302dd7701c8c41258
Piyush Suthar <32237558+PiyushSuthar@users.noreply.github.com>
2021-07-07 22:57:25 +0530

    :building_construction: removed: hook for dark mode

--- src/Components/StartMenu/index.tsx
+++ src/Components/StartMenu/index.tsx
@@ -2,7 +2,6 @@ import { useStore } from "nanostores/preact";
 import { useEffect, useRef } from "preact/hooks";
 import useClickOutside from "../../hooks/useClickOutside";
 import { useFocusOutside } from "../../hooks/useFocusOutside";
-import { ThemeStore } from "../../store/darkMode";
 import {
   showStartMenu as isStartMenuVisible,
   toggleStartMenu,
@@ -10,30 +9,29 @@ import {
 import { SearchBar } from "./SearchBar";
 import { PinnedApps } from "./PinnedApps";
 import styles from "./StartMenu.module.css";
+import { UserProfile } from "./UserProfile";
+import { RecommendedApps } from "./RecommendedApps";
 
 export const Startmenu = () => {
   const showStartMenu = useStore(isStartMenuVisible);
-  const theme = useStore(ThemeStore);
 
   const StartMenuRef = useRef<HTMLDivElement>();
 
-  // useClickOutside(StartMenuRef, () => toggleStartMenu());
+  useClickOutside(StartMenuRef, () => toggleStartMenu());
 
   return showStartMenu ? (
-    <div
-      ref={StartMenuRef}
-      style={{
-        "--start-bg": `var(${
-          theme === "dark" ? "--background_dark" : "--background_light"
-        })`,
-      }}
-      class={styles.container}
-    >
+    <div ref={StartMenuRef} class={styles.container}>
       <div class={styles.inner_container}>
         <SearchBar />
       </div>
       <div class={styles.inner_container}>
         <PinnedApps />
+      </div>
+      <div class={styles.inner_container}>
+        <RecommendedApps />
+      </div>
+      <div class={styles.user_container}>
+        <UserProfile />
       </div>
     </div>
   ) : null;
