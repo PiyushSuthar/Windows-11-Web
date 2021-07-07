a9ca6e7115aa1ebfc2f49d0b14f323ae859e1aaa
Piyush Suthar <32237558+PiyushSuthar@users.noreply.github.com>
2021-07-07 22:58:29 +0530

    :alembic: added: user profile section in Start Menu

---
+++ src/Components/StartMenu/UserProfile/index.tsx
@@ -0,0 +1,35 @@
+// import { ShutDownIcon } from "../../../assets/icons/startmenu";
+import styles from "./UserProfile.module.css";
+
+export const UserProfile = () => {
+  return (
+    <div class={styles.container}>
+      <div class={styles.user}>
+        <img
+          src="https://pbs.twimg.com/profile_images/1411205961734254594/8gSRqpri_400x400.jpg"
+          alt="Piyush Suthar"
+        />
+        <p>Piyush Suthar</p>
+      </div>
+      <ShutDownIcon />
+    </div>
+  );
+};
+
+export const ShutDownIcon = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="20"
+    height="20"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    stroke-width="2"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+    class="feather feather-power"
+  >
+    <path d="M18.36 6.64a9 9 0 1 1-12.73 0"></path>
+    <line x1="12" y1="2" x2="12" y2="12"></line>
+  </svg>
+);
