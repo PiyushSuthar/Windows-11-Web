e678b4fae4f31fcb290cb5c5474157a8b7f659f4
Piyush Suthar <32237558+PiyushSuthar@users.noreply.github.com>
2021-07-07 22:54:49 +0530

    :bento: added: icons for Start menu

---
+++ src/assets/icons/startmenu/index.tsx
@@ -0,0 +1,43 @@
+import GitHubIcon from "./icons8-github.svg";
+import TwitterIcon from "./icons8-twitter.svg";
+import PhotosIcon from "./icons8-photos.svg";
+import ChromeIcon from "./icons8-chrome.svg";
+import MoviesIcon from "./icons8-movies-tv.svg";
+import PhotoShopIcon from "./icons8-adobe-photoshop.svg";
+import MSEdgeIcon from "./icons8-microsoft-edge.svg";
+import MSExcelIcon from "./icons8-microsoft-excel-2019.svg";
+import MSStoreIcon from "./icons8-microsoft-store.svg";
+import MSWordIcon from "./icons8-microsoft-word-2019.svg";
+import MSGrooveIcon from "./icons8-microsoft-groove.svg";
+import VSCodeIcon from "./icons8-visual-studio-code-2019.svg";
+import StartMenuIcon from "./icons8-windows-10.svg";
+import YourHelpIcon from "./icons8-your-help.svg";
+import MSPPTIcon from "./icons8-microsoft-powerpoint-2019.svg";
+import MSTeamsIcon from "./icons8-microsoft-teams-2019.svg";
+import WeatherIcon from "./icons8-weather.svg";
+import WhiteBoardIcon from "./icons8-whiteboard.svg";
+import SolitareIcon from "./icons8-microsoft-solitaire-collection.svg";
+import ShutDownIcon from "./power.svg";
+
+export {
+  GitHubIcon,
+  TwitterIcon,
+  PhotosIcon,
+  ChromeIcon,
+  MoviesIcon,
+  PhotoShopIcon,
+  VSCodeIcon,
+  MSEdgeIcon,
+  MSExcelIcon,
+  MSStoreIcon,
+  MSGrooveIcon,
+  MSWordIcon,
+  StartMenuIcon,
+  YourHelpIcon,
+  MSPPTIcon,
+  MSTeamsIcon,
+  WeatherIcon,
+  WhiteBoardIcon,
+  SolitareIcon,
+  ShutDownIcon,
+};
