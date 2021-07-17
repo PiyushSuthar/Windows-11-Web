409b3a7585f1ed45c0df1b65ed7bec9cf1d6a5c2
Piyush Suthar <32237558+PiyushSuthar@users.noreply.github.com>
2021-07-17 20:59:38 +0530

    :building_construction: double click works!

--- src/Components/DesktopItems/Item/index.tsx
+++ src/Components/DesktopItems/Item/index.tsx
@@ -2,27 +2,46 @@ import styles from "./DesktopItem.module.css";
 import { Item } from "../../../Configs/DesktopItems.config";
 import { useRef, useState } from "preact/hooks";
 import useClickOutside from "../../../hooks/useClickOutside";
+import { useStore } from "nanostores/preact";
+import { OpenApps } from "../../../store/activeWindow";
 // import { FileContextMenu } from "../../../Configs/ContextMenus/file.config";
 // import { ContextMenu } from "../../ContextMenu";
 
 export const DesktopItem = (props: Item) => {
   const [isActive, setIsActive] = useState(false);
   const ItemRef = useRef<HTMLDivElement>();
+  const OpenedApps = useStore(OpenApps);
 
   useClickOutside(ItemRef, () => setIsActive(false));
 
+  const onDBClickAction =
+    props.action ||
+    (() => {
+      OpenApps.set({
+        ...OpenedApps,
+        [props.appId!]: {
+          ...(OpenedApps[props.appId!] || {}),
+          isActive: true,
+        },
+      });
+      setIsActive(false);
+    });
   return (
     <>
       {/* <ContextMenu items={FileContextMenu} containerRef={ItemRef} /> */}
       <div
         ref={ItemRef}
+        id="desktop-item"
         onClick={() => setIsActive(true)}
+        onDblClick={onDBClickAction}
         class={[styles.desktop_item, isActive && styles.active].join(" ")}
       >
-        <div className={styles.desktop_item_icon}>
-          <img src={props.icon} alt={props.name} />
+        <div id="desktop-item" className={styles.desktop_item_icon}>
+          <img id="desktop-item" src={props.icon} alt={props.name} />
         </div>
-        <div class={styles.desktop_item_name}>{props.name}</div>
+        <div id="desktop-item" class={styles.desktop_item_name}>
+          {props.name}
+        </div>
       </div>
     </>
   );
