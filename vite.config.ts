3ae7e5ecc210d74b7f3c8885e1166fc15acbea01
Piyush Suthar <32237558+PiyushSuthar@users.noreply.github.com>
2021-07-18 19:33:57 +0530

    :alembic:testing: compression

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import preact from '@preact/preset-vite'
+import viteCompression from 'vite-plugin-compression';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [preact()],
+  plugins: [viteCompression({
+    algorithm: 'brotliCompress'
+  }), preact()],
 })
