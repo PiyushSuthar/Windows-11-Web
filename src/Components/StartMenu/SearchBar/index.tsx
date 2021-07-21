63e2aa4db56aec01e02482e2e5f011ebda1282ac
Piyush Suthar <32237558+PiyushSuthar@users.noreply.github.com>
2021-07-21 22:36:38 +0530

    :children_crossing: making search bar reusable

--- src/Components/StartMenu/SearchBar/index.tsx
+++ src/Components/StartMenu/SearchBar/index.tsx
@@ -1,16 +1,31 @@
 import styles from "./SearchBar.module.css";
 import { SearchIcon } from "./searchIcon";
 
-export const SearchBar = () => {
+interface Props {
+  placeholder?: string;
+  onSearch?: (query: string) => void;
+  className: string;
+}
+export const SearchBar = ({
+  placeholder = "Type here to search",
+  onSearch,
+  className,
+}: Props) => {
   return (
-    <div class={styles.search_container}>
+    <div class={[styles.search_container, className].join(" ")}>
       <div class={styles.icon}>
         <SearchIcon />
       </div>
       <input
         class={styles.search_input}
         type="text"
-        placeholder="Type here to search"
+        placeholder={placeholder}
+        onKeyPress={(e: KeyboardEvent) => {
+          if (e.key === "Enter") {
+            const query = (e.target as HTMLInputElement).value;
+            query && onSearch && onSearch(query);
+          }
+        }}
       />
     </div>
   );
