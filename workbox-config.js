7165a19df119cdd440bb4939d36eded111f3a22a
Piyush Suthar <32237558+PiyushSuthar@users.noreply.github.com>
2021-07-24 16:50:37 +0530

    :hammer: added: more file types

--- workbox-config.js
+++ workbox-config.js
@@ -1,5 +1,5 @@
 module.exports = {
     globDirectory: 'dist/',
-    globPatterns: ['**/*.{js,css,ico,html,svg,mp3,webp}'],
+    globPatterns: ['**/*.{js,css,ico,html,svg,mp3,webp,png,jpg}'],
     swDest: 'dist/sw.js',
 };
