0d7e7a80be7d332eea523cdc8e129149e56b39c5
alixsep <alixsep@outlook.com>
2021-07-28 09:33:22 +0430

    :small_blue_diamond: Fix explorer and search icon
    Fix file explorer icon
    Fix search icon
    Allow search to open start menu

--- src/Components/Taskbar/index.tsx
+++ src/Components/Taskbar/index.tsx
@@ -3,11 +3,16 @@ import { ICONS } from "../../Configs/Taskbar.config";
 import { ThemeStore, toggleTheme } from "../../store/darkMode";
 import styles from "./taskbar.module.css";
 import { TaskBarButton } from "./TaskbarButton";
+import startMenu from '../../assets/icons/taskbar/windows.png'
+import dark_mode_search_icon from "../../assets/icons/taskbar/search-dark.svg";
+import light_mode_search_icon from "../../assets/icons/taskbar/search-light.svg";
 import dark_mode_icon from "../../assets/icons/taskbar/dark_mode.png";
 import light_mode_icon from "../../assets/icons/taskbar/light_mode.png";
 import { AppsConfig } from "../../Configs/apps.config";
 import { ActionCenter } from "./ActionCenter";
 import { OpenApps } from "../../store/activeWindow";
+import { toggleStartMenu } from "../../store/startMenu";
+
 interface Props {}
 
 export const TaskBar = (props: Props) => {
@@ -15,9 +20,19 @@ export const TaskBar = (props: Props) => {
   const OpenedApps = useStore(OpenApps);
 
   return (
-    <div class={styles.container}>
-      <div class={styles.inner_container}>
+    <div className={styles.container}>
+      <div className={styles.inner_container}>
         <div className={styles.icons}>
+          <TaskBarButton
+            url={startMenu}
+            name="Start menu"
+            action={toggleStartMenu}
+          />
+          <TaskBarButton
+            url={theme === "dark" ? dark_mode_search_icon : light_mode_search_icon}
+            name="Search"
+            action={toggleStartMenu}
+          />
           {ICONS.map((icon, index) => (
             <TaskBarButton {...icon} key={index} />
           ))}
