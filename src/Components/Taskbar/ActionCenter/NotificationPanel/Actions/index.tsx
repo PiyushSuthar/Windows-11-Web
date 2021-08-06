89e431054cb20f2e70b4f515972c3569b4627e71
Piyush Suthar <32237558+PiyushSuthar@users.noreply.github.com>
2021-08-06 22:46:16 +0530

    :children_crossing: added: actions to action center

--- src/Components/Taskbar/ActionCenter/NotificationPanel/Actions/index.tsx
+++ src/Components/Taskbar/ActionCenter/NotificationPanel/Actions/index.tsx
@@ -1,7 +1,79 @@
+import preact, { FunctionalComponent } from "preact";
+import { useState } from "preact/hooks";
+import { ChevronIcon } from "../../../../StartMenu/PinnedApps";
+import { AccessibilityIcon } from "../../icons/accessibilityIcon";
+import { AirplaneIcon } from "../../icons/airplane";
+import { BatterySaverIcon } from "../../icons/battery";
+import { BluetoothIcon } from "../../icons/bluetooth";
+import { MoonIcon } from "../../icons/moon";
+import { WifiIcon } from "../../icons/wifi";
+import styles from "./Action.module.css";
+
+interface IAction {
+  icon: (props: { size: string }) => any;
+  name: string;
+  show_chevron?: boolean;
+  default_state?: boolean;
+}
+
+const ACTIONS: IAction[] = [
+  {
+    icon: WifiIcon,
+    name: "Wifi",
+    show_chevron: true,
+    default_state: true,
+  },
+  {
+    icon: BluetoothIcon,
+    name: "Bluetooth",
+    show_chevron: true,
+  },
+  {
+    icon: AirplaneIcon,
+    name: "Airplane Mode",
+  },
+  {
+    icon: BatterySaverIcon,
+    name: "Battery Saver",
+  },
+  {
+    icon: MoonIcon,
+    name: "Focus Assist",
+  },
+  {
+    icon: AccessibilityIcon,
+    name: "Accessibility",
+    show_chevron: true,
+  },
+];
+
 export const Actions = () => {
   return (
-    <div className="container">
-      <div className="inner"></div>
+    <div class={styles.container}>
+      {ACTIONS.map((action) => (
+        <Action {...action} key={action.name} />
+      ))}
+    </div>
+  );
+};
+
+export const Action: FunctionalComponent<IAction> = ({
+  name,
+  icon: Icon,
+  default_state,
+  show_chevron,
+}) => {
+  const [isActive, setIsActive] = useState(default_state || false);
+  return (
+    <div
+      onClick={() => setIsActive((prev) => !prev)}
+      class={styles.action_container}
+    >
+      <div class={[styles.icon, isActive && styles.active].join(" ")}>
+        <Icon size="20" />
+        {show_chevron && <ChevronIcon size="18" />}
+      </div>
+      {name}
     </div>
   );
 };
