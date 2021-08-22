b585540ce3a4f873c0cfe1ef8a101bc33f2f3ece
Piyush Suthar <32237558+PiyushSuthar@users.noreply.github.com>
2021-08-22 15:44:23 +0530

    :alembic: added: Vscode app

---
+++ src/Apps/VsCode/index.tsx
@@ -0,0 +1,14 @@
+export const VsCodeApp = () => {
+  return (
+    <iframe
+      src="https://github1s.com/PiyushSuthar/Windows-11-Web"
+      title="Windows 11 Web"
+      width="100%"
+      height="100%"
+      style={{
+        borderRadius: "0 0 6px 6px",
+        border: "none",
+      }}
+    />
+  );
+};
