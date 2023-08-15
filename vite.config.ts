6cbc851bd5630ee95f0376fe801a19f7b2cb7313
Piyush Suthar <piyushsuthar100@gmail.com>
2023-08-15 12:45:53 +0530

    updated: to million js

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,14 @@
 import { defineConfig } from 'vite'
 import preact from '@preact/preset-vite'
 import viteCompression from 'vite-plugin-compression';
+import million from 'million/compiler'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [viteCompression({
+  plugins: [million.vite({
+    auto: true,
+    mode: 'preact',
+  }), viteCompression({
     algorithm: 'brotliCompress'
   }), preact()],
 })
